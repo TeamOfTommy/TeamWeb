4248b59df9e008801ddfc246dde26d5051f077cf
LiJIayue <1558488058@qq.com>
2016-09-27 11:34:28 +0800

    modify router

--- Public/assest/js/home/Home.jsx
+++ Public/assest/js/home/Home.jsx
@@ -18,8 +18,7 @@ export default class Home extends React.Component {
         return (
             <Router history={browserHistory}>
                 <Route path={pathConfig.rootPath} component={HomeLayout}>
-                    {/* <IndexRoute component={FirstBlock}/> */}
-                    <IndexRoute component={ThirdBlock}/>
+                    <IndexRoute component={FirstBlock}/>
                     <Route path="/second" component={SecondBlock}/>
                     <Redirect from="second" to="/second"/>
                     <Route path="/third" component={ThirdBlock}/>
